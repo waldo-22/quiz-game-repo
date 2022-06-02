06fb4e6f6ccffde261aee243a9c7497b3eaa2858
Reniel Christianson <waldochristianson@gmail.com>
2022-06-02 09:58:00 -0500

    quiz game updated

--- script.js
+++ script.js
@@ -3,12 +3,13 @@ const startButton = document.getElementById('start-btn')
 // let timer = document.getElementById("timer");
 // **adding containers to quiz questions and answers**//
 let quizContainer = document.getElementById("#quiz");
-let question = document.querySelector("#question");
+let question = document.querySelector("#quizQuestion");
 let choiceA = document.querySelector("#choiceA");
 let choiceB = document.querySelector("#choiceB");
 let choiceC = document.querySelector("#choiceC");
 let choiceD = document.querySelector("#choiceD");
-
+// let displayQuestion = document.querySelector("#nextQuestion")
+let mainDiv = document.querySelector('#main');
 // choiceA.addEventListener("click", answerChoiceA);
 // choiceB.addEventListener("click", answerChoiceB);
 // choiceC.addEventListener("click", answerChoiceC);
@@ -18,63 +19,106 @@ let choiceD = document.querySelector("#choiceD");
 
 //***Score Container***
 let scoreContainer = document.getElementById("#score");
-let score = 0; 
+let score = 0;
 
 // *****Questions with answers and choices*****//
+// console.log(myQuestions[])
+let questionIndex = 0
+console.log(questionIndex)
 const myQuestions = [
-    {
-            question: "You have 10 fingers but 3 thumbs get chopped off. How many fingers do you have left?",
-            choiceA: "7",
-            choiceB: "9",
-            choiceC: "3",
-            choiceD: "I dont have 3 thumbs",
-            answer: "I dont have 3 thumbs",
-    },
+        {
+                question: "You have 10 fingers but 3 thumbs get chopped off. How many fingers do you have left?",
+                choiceA: "7",
+                choiceB: "9",
+                choiceC: "3",
+                choiceD: "I dont have 3 thumbs",
+                answer: "I dont have 3 thumbs",
+        },
 
-    {
-            question: "How many years are in a decade?",
-            choiceA: "12yrs",
-            choiceB: "10yrs",
-            choiceC: "100trs",
-            choiceD: "50yrs",
-            answer: "10yrs",
-    },
+        {
+                question: "How many years are in a decade?",
+                choiceA: "12yrs",
+                choiceB: "10yrs",
+                choiceC: "100yrs",
+                choiceD: "50yrs",
+                answer: "10yrs",
+        },
 
-    {
-            question: "What month is Black History month?",
-            choiceA: "February",
-            choiceB: "May",
-            choiceC: "December",
-            choiceD: "July",
-            answer: "February",
-    },
+        {
+                question: "What month is Black History month?",
+                choiceA: "February",
+                choiceB: "May",
+                choiceC: "December",
+                choiceD: "July",
+                answer: "February",
+        },
 
-    {
-            question: "How many grams are in an Ounce?",
-            choiceA: "14grams",
-            choiceB: "25grams",
-            choiceC: "28grams",
-            choiceD: "20grams",
-            answer: "28grams",
-    },
+        {
+                question: "Where is waldo?",
+                choiceA: "Here",
+                choiceB: "There",
+                choiceC: "Next to me",
+                choiceD: "Everywhere",
+                answer: "Everywhere",
+        },
 
-    {
-            question: "By the age of 18years old, how many days have you lived?",
-            choiceA: "6,500 days",
-            choiceB: "18 days",
-            choiceC: "5,378 days",
-            choiceD: "6,570 days",
-            answer: "6,570 days ",
-    }
+        {
+                question: "By the age of 18years old, how many days have you lived?",
+                choiceA: "6,500 days",
+                choiceB: "18 days",
+                choiceC: "5,378 days",
+                choiceD: "6,570 days",
+                answer: "6,570 days ",
+        }
 
 ]
 // **pair start button with begin button**//
 
-startButton.addEventListener("click", beginQuiz); 
+startButton.addEventListener("click", beginQuiz);
+
+// displayQuestion.addEventListener("click", newQuestion);
+
+mainDiv.addEventListener("click", answerButtons)
+
+function answerButtons(event) {
+       
+
+
+        if (event.target.matches(".answer")){
+                console.log(event.target)
+                console.log(questionIndex)
+                console.log(event.target.innerText)
+                // add condition 
+                if (event.target.innerText == myQuestions[questionIndex].answer){
+                        score += 1
+                        console.log(score)
+
+                }
+                // showQuestion()
+                questionIndex += 1
+                console.log("wait")
+                question.innerText = myQuestions[questionIndex].question;
+        choiceA.innerText = myQuestions[questionIndex].choiceA;
+        choiceB.innerText = myQuestions[questionIndex].choiceB;
+        choiceC.innerText = myQuestions[questionIndex].choiceC;
+        choiceD.innerText = myQuestions[questionIndex].choiceD;
+
+
+
+        }
+}
+
+
+
+
 // *****add function to start quiz and timer*****//
 function beginQuiz() {
-    countdown();
-    showQuestion();
+        countdown();
+        question.innerText = myQuestions[questionIndex].question;
+        choiceA.innerText = myQuestions[questionIndex].choiceA;
+        choiceB.innerText = myQuestions[questionIndex].choiceB;
+        choiceC.innerText = myQuestions[questionIndex].choiceC;
+        choiceD.innerText = myQuestions[questionIndex].choiceD;
 }
 // *Timer function*
 
@@ -82,30 +126,26 @@ let timer = 60
 let quizTimer = document.querySelector("#quiz-timer")
 function countdown() {
 
-    var timeLeft = setInterval(() => {
-  
-      if (timer >= 0 || questionIndex < 5) {
-        quizTimer.textContent = timer + " seconds left";
-        timer--;
-        //questionIndex ++;
-  
-      } else {
-        clearInterval(timeLeft);
-  
-        
-      }
-    }, 1000);
-  }
-  
+        var timeLeft = setInterval(() => {
+                if (timer >= 0 || questionIndex < 5) {
+                        quizTimer.textContent = timer + " seconds left";
+                        timer--;
+                        //questionIndex ++;
+                } else {
+                        clearInterval(timeLeft);
+                }
+        }, 1000);
+}
 
 // **Dynamically changes states of questions and choices**//
 
 // function showQuestion() {
-//     question.innerText = myQuestions[i].question;
-//     choiceA.innerText = myQuestions[i].choiceA
-//     choiceB.innerText = myQuestions[i].choiceB
-//     choiceC.innerText = myQuestions[i].choiceC
-//     choiceD.innerText = myQuestions[i].choiceD
+//         console.log(questionIndex)
+//         question.innerText = myQuestions[questionIndex].question;
+//         choiceA.innerText = myQuestions[questionIndex].choiceA;
+//         choiceB.innerText = myQuestions[questionIndex].choiceB;
+//         choiceC.innerText = myQuestions[questionIndex].choiceC;
+//         choiceD.innerText = myQuestions[questionIndex].choiceD;
 // }
 
 var timerElement = document.querySelector(".timer-count");
